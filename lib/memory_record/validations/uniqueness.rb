b5ae81579025583f92451940b61336fe7874999f
Dan Cunning <dancunning@gmail.com>
2013-07-17 16:12:10 -0400

    validates_uniqueness_of uses the primary key index if it's available

--- lib/memory_record/validations/uniqueness.rb
+++ lib/memory_record/validations/uniqueness.rb
@@ -11,16 +11,23 @@ module MemoryRecord
       # Unfortunately, we have to tie Uniqueness validators to a class.
       def setup(klass)
         @klass = klass
+        @primary_key = klass.table.primary_key
       end
       
       def validate_each(record, attribute, value)
-        query = @klass.where(attribute => record.send(attribute))
-        
-        unless record.new_record?
-          query = query.remove_if {|r| r == record }
+        if attribute.to_s == @primary_key && record.new_record?
+          is_taken = @klass.send("find_by_#{@primary_key}") != nil
+        else
+          query = @klass.where(attribute => record.send(attribute))
+
+          unless record.new_record?
+            query = query.remove_if {|r| r == record }
+          end
+
+          is_taken = !query.empty?
         end
         
-        unless query.empty?
+        if is_taken
           record.errors.add(attribute, :taken, options.except(:case_sensitive, :scope).merge(:value => value))
         end
       end
